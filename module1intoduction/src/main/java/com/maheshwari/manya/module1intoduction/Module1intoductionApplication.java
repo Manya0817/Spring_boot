2a45ecea31fb1700497d4739e42902a4a4f775a8
Manya0817 <maheshwarimanya7@gmail.com>
2026-01-14 22:28:54 +0530

    added module 1 intro

---
+++ module1intoduction/src/main/java/com/maheshwari/manya/module1intoduction/Module1intoductionApplication.java
@@ -0,0 +1,13 @@
+package com.maheshwari.manya.module1intoduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1intoductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1intoductionApplication.class, args);
+	}
+
+}
---
+++ module1intoduction/src/test/java/com/maheshwari/manya/module1intoduction/Module1intoductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maheshwari.manya.module1intoduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1intoductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
