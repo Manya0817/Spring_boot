c3338959450950f06d9b9a1ed0f6926f31701b95
Manya0817 <maheshwarimanya7@gmail.com>
2026-01-25 22:52:53 +0530

    Learned 2.4 About the Service Layer

--- Module2/src/main/java/com/Maheshwari/Manya/Module2/controllers/EmployeeController.java
+++ Module2/src/main/java/com/Maheshwari/Manya/Module2/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.Maheshwari.Manya.Module2.controllers;
 import com.Maheshwari.Manya.Module2.dto.EmployeeDTO;
 import com.Maheshwari.Manya.Module2.entities.EmployeeEntity;
 import com.Maheshwari.Manya.Module2.repositories.EmployeeRepository;
+import com.Maheshwari.Manya.Module2.services.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,30 +18,37 @@ public class EmployeeController {
 //        return "Secret message : your_secret";
 //    }
 
-    private final EmployeeRepository employeeRepository;
+//    private final EmployeeRepository employeeRepository;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
+//    public EmployeeController(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+
     @GetMapping(path="/{employeeId}")
-    public EmployeeEntity getEmployeeById(@PathVariable(name="employeeId") Long id){
+    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id){
 //        return new EmployeeDTO(id,"Manya","maheshwarimanya@gmail.com",22, LocalDate.of(2026,01,23),true);
-        return employeeRepository.findById(id).orElse(null);
+//        return employeeRepository.findById(id).orElse(null);
+        return employeeService.getEmployeeById(id);
     }
 
     @GetMapping
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false,name="inputAge") Integer age,
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false,name="inputAge") Integer age,
                                                 @RequestParam(required = false) String sortBy){
 //        return "Hi age "+age+" "+sortBy;
-        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
     @PostMapping
-    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee){
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
 //        inputEmployee.setId(100L);
 //        return inputEmployee;
-        return employeeRepository.save(inputEmployee);
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping String updateEmployeeById(){
