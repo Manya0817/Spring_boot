ee842f168b31f0983f08c047a4a4484ae9b08cc5
Manya0817 <maheshwarimanya7@gmail.com>
2026-02-11 17:38:52 +0530

    Completed 3.6 learned about Entity Life Cycle

--- module3.5/src/main/java/com/hospitalManagementSystem/module35/service/PatientService.java
+++ module3.5/src/main/java/com/hospitalManagementSystem/module35/service/PatientService.java
@@ -7,7 +7,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class PatientService {
-
+    private final PatientRepository patientRepository;
+    public void testPatientTransaction(){
+        Patient p1=patientRepository.findById(1L).orElseThrow();
+        Patient p2=patientRepository.findById(1L).orElseThrow();
+        System.out.println(p1+" "+p2);
+        System.out.println(p1==p2);
+        p1.setName("Random Name");
+    }
 
 }
--- module3.5/src/test/java/com/hospitalManagementSystem/module35/PatientServiceTest.java
+++ module3.5/src/test/java/com/hospitalManagementSystem/module35/PatientServiceTest.java
@@ -5,6 +5,7 @@ import com.hospitalManagementSystem.module35.dto.CPatientInfo;
 import com.hospitalManagementSystem.module35.dto.IPatientInfo;
 import com.hospitalManagementSystem.module35.entity.Patient;
 import com.hospitalManagementSystem.module35.repository.PatientRepository;
+import com.hospitalManagementSystem.module35.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,12 +17,17 @@ public class PatientServiceTest {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public void  testPatient(){
 //        List<Patient> patientList=patientRepository.findAll();
 //        List<CPatientInfo> patientList=patientRepository.getAllPatientsInfoConcrete();
 //
-        int rowsAffected=patientRepository.updatePatientNameWithId("Manya Maheshwari",1L);
-        System.out.println(rowsAffected);
+//        int rowsAffected=patientRepository.updatePatientNameWithId("Manya Maheshwari",1L);
+//        System.out.println(rowsAffected);
+
+        patientService.testPatientTransaction();
     }
 }
