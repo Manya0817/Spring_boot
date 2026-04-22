643c41eb7522a64f8dd4679157e986b23fafaf62
Manya0817 <maheshwarimanya7@gmail.com>
2026-04-22 21:50:41 +0530

    added module 9 spring ai

---
+++ module7/src/test/java/testing/example/module7/controllers/AbstractIntegrationTest.java
@@ -0,0 +1,32 @@
+package testing.example.module7.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import testing.example.module7.TestContainerConfiguration;
+import testing.example.module7.dto.EmployeeDto;
+import testing.example.module7.entities.Employee;
+
+@AutoConfigureWebTestClient(timeout = "100000")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainerConfiguration.class)
+public class AbstractIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    Employee testEmployee = Employee.builder()
+            .id(1L)
+            .email("anuj@gmail.com")
+            .name("Anuj")
+            .salary(200L)
+            .build();
+    EmployeeDto testEmployeeDto = EmployeeDto.builder()
+            .id(1L)
+            .email("anuj@gmail.com")
+            .name("Anuj")
+            .salary(200L)
+            .build();
+}
